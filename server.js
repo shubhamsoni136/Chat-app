6aba131d3350a9c74821ac84f4fe1d95e7f9f958
shubham <shubhamsoni136@gmail.com>
2018-01-19 19:54:55 +0530

    files added

--- server.js
+++ server.js
@@ -18,11 +18,24 @@ io.sockets.on('connection',function(socket){
   console.log('Connected:%s socket connected',connections.length);
 
   socket.on('disconnect',function(){
+    users.splice(users.indexOf(socket.username),1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket),1);
     console.log('Disconnect:%s socket connected',connections.length);
   });
 
   socket.on('send message',function(data){
-    io.sockets.emit('new message',{msg:data});  
+    io.sockets.emit('new message',{msg:data,users:socket.username});
   });
+
+  socket.on('new user',function(data,callback){
+    callback(true);
+    socket.username=data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get user',users)
+  }
 });
